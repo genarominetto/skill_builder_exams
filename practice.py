de14c7cf4918b3ac7ce6a8a5416feffab8ab91ba
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-02-13 22:36:32 +0100

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,17 +1,17 @@
 from skill_builder_exams.sql import read_all_exams
 
-def practice(tags):
-    exams = read_all_exams(tags)
-    print(str(len(exams)) + " exams selected.")
-    print()
+def practice(tags, db_path='/content/skill_builder_exams/exams.db'):
+    exams = read_all_exams(tags, db_path=db_path)
+    print(f"{len(exams)} exams selected.\n")
     import random
     random.shuffle(exams)
     for exam in exams:
-        response = input("Do you want to practice " + exam.exam_description + "? (y/n) " + str(len(exam.question_answer_dict)) + " questions.\n")
+        questions_count = len(exam.question_answer_dict)
+        response = input(f"Do you want to practice {exam.exam_description}? (y/n) {questions_count} questions.\n")
         if response.lower() == "y":
-            print("Practicing ---" + exam.exam_description + "---")
-            exam.practice_row()
+            print(f"Practicing ---{exam.exam_description}---")
+            exam.practice_row()  # Assuming this method exists within the Exam class
             print()
         else:
-            print("Skipping ---" + exam.exam_description + "---")
+            print(f"Skipping ---{exam.exam_description}---")
             print()
